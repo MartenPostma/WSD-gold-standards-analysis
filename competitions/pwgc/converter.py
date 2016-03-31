7e47b755bf0a8672891c66b82fab7d442804774d
Marten Postma <marten@marten.local>
2016-03-31 09:48:44 +0200

    added lemma.pos.pickle files for semcor30 and pwgc

--- competitions/pwgc/converter.py
+++ competitions/pwgc/converter.py
@@ -5,7 +5,7 @@ DOCUMENT_ID LEMMA.IDENTIFIER KEY1 KEY2 KEYN
 '''
 
 #lemma is of format: "great%1|great%3"
-#focus on 'gloss' and 'ex' elements
+#focus on 'gloss/def' and 'gloss/ex' elements
 #there can be more than one sense key
 
 #gunzip WordNet-3.0.gz before running this script
@@ -13,48 +13,154 @@ DOCUMENT_ID LEMMA.IDENTIFIER KEY1 KEY2 KEYN
 from lxml import etree
 from collections import defaultdict
 import gzip
+import pickle
+import os
 
-sw_template = 'synset/gloss/{category}/wf[@tag="man"]'
-mw_template = 'synset/gloss/{category}/cf/glob[@tag="man"]'
+# needed: lemma, pos, sensekey, target sent
+
+# sw_template = 'synset/gloss/{category}/wf[@tag="man"]'
+# mw_template = 'synset/gloss/{category}/cf/glob[@tag="man"]'
+lemma_pos = {}
+output_dir = 'pwgc_lemmapos2sensekeys'
+
+template = 'synset/gloss/{category}'
 basedir = 'WordNet-3.0/glosstag/merged'
 basenames = ['adj.xml.gz', 'adv.xml.gz','noun.xml.gz','verb.xml.gz']
-paths = [ (basename[:-4], '/'.join([basedir,basename]))
+paths = [ (basename[:-3], '/'.join([basedir,basename]))
           for basename in basenames]
-categories = ['def','ex']
+categories = ['def', 'ex']
 
+training_freq = defaultdict(int)
+tokens = 0
 
-stats = defaultdict(int)
+def get_lemma_pos_of_sensekey(sense_key):
+    """
+    lemma and pos are determined for a wordnet sense key
+
+    .. doctest::
+        >>> get_lemma_pos_of_sensekey('life%1:09:00::')
+        ('life','n')
+
+    :param str sense_key: wordnet sense key
+
+    :rtype: tuple
+    :return: (lemma, n | v | r | a | u)
+    """
+    if '%' not in sense_key:
+        return '', 'u'
+
+    lemma, information = sense_key.split('%')
+    int_pos = information[0]
+
+    if int_pos == '1':
+        this_pos = 'n'
+    elif int_pos == '2':
+        this_pos = 'v'
+    elif int_pos in {'3', '5'}:
+        this_pos = 'a'
+    elif int_pos == '4':
+        this_pos = 'r'
+    else:
+        this_pos = 'u'
+
+    return lemma, this_pos
 
 with open('answers','w') as outfile:
     for doc_id, path in paths:
         doc = etree.parse(gzip.open(path))
         for category in categories:
-            for template in [sw_template,mw_template]:
                 xpath_expr = template.format(**locals())
                 for el in doc.xpath(xpath_expr):
 
-                    iden = el.get('id')
-                    sensekeys = [id_el.get('sk')
-                                 for id_el in el.iterfind('id')]
+                    if el.tag == 'def':
+                        children = el.getchildren()
 
-                    if not sensekeys:
-                        continue
+                    elif el.tag == 'ex':
+                        children = []
+                        for child_el in el.getchildren():
+                            if child_el.tag == 'qf':
+                                children.extend(child_el.getchildren())
+                            else:
+                                children.append(child_el)
 
+                    # set default values
+                    sentence = []
+                    key2indices = defaultdict(set)
+                    index = 0
+                    mw = False
 
-                    key_string = ' '.join(sensekeys)
-                    output = '{doc_id} {iden} {key_string}\n'.format(**locals())
-                    outfile.write(output)
+                    for index, child_el in enumerate(children):
 
-                    stats[(category,template)] += 1
-
-for key,value in stats.items():
-    print(key,value)
-
-print('totat: %s' % sum(stats.values()))
-
-
-
-
+                        tokens += 1
 
 
+                        if child_el.tag == 'cf':
+                            mw = True
+                            glob_el = child_el.find('glob')
 
+                            if glob_el is None:
+                                token = child_el.text
+                            else:
+                                token = glob_el.tail
+
+                            sentence.append(token)
+
+                        elif child_el.tag == 'wf':
+                            iden = child_el.get('id')
+                            id_els = child_el.findall('id')
+                            tag = child_el.get('tag')
+
+                            if all([id_els,
+                                    tag in {'auto', 'man'}]):
+
+                                token = id_els[-1].tail
+
+                                if tag == 'man':
+                                    sensekeys = [id_el.get('sk')
+                                                 for id_el in id_els]
+                                    lemma = id_els[0].get('lemma')
+
+                                    for sensekey in sensekeys:
+                                        training_freq[sensekey] += 1
+                                        ir, pos = get_lemma_pos_of_sensekey(sensekey)
+
+                                        key2indices[(sensekey, pos)].add((index, lemma))
+
+                                    key_string = ' '.join(sensekeys)
+                                    output = '{doc_id} {iden} {key_string}\n'.format(**locals())
+                                    outfile.write(output)
+
+                            else:
+                                token = child_el.text
+
+                            sentence.append(token)
+
+                    for (sensekey, pos), info in key2indices.items():
+                        for (index, lemma) in info:
+
+                            target_sent = ['<head>%s</head>' % token
+                                           if this_index == index else token
+                                           for this_index, token in
+                                           enumerate(sentence)]
+
+                            target_sent = ' '.join(target_sent)
+
+                            if (lemma, pos) not in lemma_pos:
+                                lemma_pos[(lemma, pos)] = defaultdict(set)
+
+                            lemma_pos[(lemma, pos)][sensekey].add(target_sent)
+
+                    # reset variables
+                    sentence = []
+                    key2indices = defaultdict(set)
+                    index = 0
+
+
+# write all lemma_pos pickle to file
+for (lemma, pos), sensekeys in lemma_pos.items():
+    path = os.path.join(output_dir, lemma + '___' + pos + '.pickle')
+    with open(path, 'wb') as outfile:
+        pickle.dump(sensekeys, outfile)
+
+with open('freq_training_examples.pickle', 'wb') as outfile:
+    pickle.dump(training_freq, outfile)
--- competitions/semcor30/converter.py
+++ competitions/semcor30/converter.py
@@ -7,9 +7,77 @@ import os
 import glob
 from collections import defaultdict
 import pickle
+import re
 
 cwd = os.getcwd()
 training_freq = defaultdict(int)
+lemma_pos = {}
+output_dir = 'semcor30_lemmapos2sensekeys'
+
+
+
+def get_lemma_pos_of_sensekey(sense_key):
+    """
+    lemma and pos are determined for a wordnet sense key
+
+    .. doctest::
+        >>> get_lemma_pos_of_sensekey('life%1:09:00::')
+        ('life','n')
+
+    :param str sense_key: wordnet sense key
+
+    :rtype: tuple
+    :return: (lemma, n | v | r | a | u)
+    """
+    if '%' not in sense_key:
+        return '', 'u'
+
+    lemma, information = sense_key.split('%')
+    int_pos = information[0]
+
+    if int_pos == '1':
+        this_pos = 'n'
+    elif int_pos == '2':
+        this_pos = 'v'
+    elif int_pos in {'3', '5'}:
+        this_pos = 'a'
+    elif int_pos == '4':
+        this_pos = 'r'
+    else:
+        this_pos = 'u'
+
+    return lemma, this_pos
+
+def parse_line_with_sense_key(line, doc_id):
+    """
+    given a line from semcor30 like this:
+    <wf cmd=done pos=JJ lemma=recent wnsn=2 lexsn=5:00:00:past:00>recent</wf>
+
+    :param str line: line from semcor
+    :param str doc_id: sourc document basename
+
+    :rtype: tuple
+    :return: sensekey, output_line_semeval
+    """
+    relevant = line.split('>')[0]
+
+    info = {item.split('=')[0]: item.split('=')[1]
+            for item in relevant.split(' ')
+            if '=' in item}
+
+    lemma = info['lemma']
+    sense_key = info['lemma'] + '%' + info['lexsn']
+    irrelevant, pos = get_lemma_pos_of_sensekey(sense_key)
+
+
+    iden = doc_id + '.' + str(counter)
+    output = '{doc_id} {lemma}.{iden} {sense_key}\n'.format(**locals())
+
+    return lemma, sense_key, pos, output
+
+sentence = []
+key2indices = defaultdict(set)
+index = 0
 
 
 with open('answers','w') as outfile:
@@ -21,25 +89,56 @@ with open('answers','w') as outfile:
             with open(path) as infile:
                 doc_id = os.path.basename(path)
                 for counter,line in enumerate(infile):
-                    if all([line.startswith('<wf'),
-                            'lexsn' in line]):
 
-                        relevant = line.split('>')[0]
-
-                        info = {item.split('=')[0]: item.split('=')[1]
-                                 for item in relevant.split(' ')
-                                 if '=' in item}
-
-                        lemma = info['lemma']
-                        sense_key = info['lemma'] + '%' + info['lexsn']
-
-                        training_freq[sense_key] += 1
-
-                        iden = doc_id + '.' + str(counter)
-                        output = '{doc_id} {lemma}.{iden} {sense_key}\n'.format(**locals())
-                        outfile.write(output)
+                    if line.startswith('<punc>'):
+                        token = re.findall('<punc>(.*)</punc>', line)[0]
+                        sentence.append(token)
+                        index += 1
 
 
+                    elif line.startswith('<wf'):
+                        token = re.findall('>(.*)</wf>', line)[0]
+                        sentence.append(token)
+                        if all([line.startswith('<wf'),
+                                'lexsn' in line]):
+                            lemma, sensekey, pos, output_line = parse_line_with_sense_key(line,
+                                                                                          doc_id)
+                            outfile.write(output_line)
+                            training_freq[sensekey] += 1
+                            key2indices[(sensekey, pos)].add((index, lemma))
+                        index += 1
+
+                    elif line.startswith('<s'):
+                        sent_id = line.strip()[:-1].replace('<s snum=', '')
+
+                    elif line.startswith('</s'):
+                        for (sensekey, pos), info in key2indices.items():
+                            for (index, lemma) in info:
+
+                                target_sent = ['<head>%s</head>' % token
+                                               if this_index == index else token
+                                               for this_index,token in enumerate(sentence)]
+
+                                target_sent = ' '.join(target_sent)
+
+                                if (lemma, pos) not in lemma_pos:
+                                    lemma_pos[(lemma, pos)] = defaultdict(set)
+
+                                lemma_pos[(lemma, pos)][sensekey].add(target_sent)
+
+                        # reset variables
+                        sentence = []
+                        key2indices = defaultdict(set)
+                        index = 0
+
+
+# write all lemma_pos pickle to file
+for (lemma, pos), sensekeys in lemma_pos.items():
+    path = os.path.join(output_dir, lemma + '___' + pos + '.pickle')
+    with open(path, 'wb') as outfile:
+        pickle.dump(sensekeys, outfile)
+
+# write frequency to file
 with open('freq_training_examples.pickle', 'wb') as outfile:
     pickle.dump(training_freq, outfile)
 
